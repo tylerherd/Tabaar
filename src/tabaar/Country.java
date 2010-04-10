6a551c8268f594c722a6d6003616f98975fa6c46
Brandon Lyons <t.brandon.lyons@gmail.com>
2010-04-10 09:33:15 -0600

    Added member variable mOwner that will keep track of the player occupying the country Added member variable mArmies that will keep track of the number of armies occupying the country

--- src/tabaar/Country.java
+++ src/tabaar/Country.java
@@ -7,4 +7,16 @@ package tabaar;
 public class Country
 {
 
+   /**
+    * mOwner is a reference to the Commander object that currently owns this
+    * country. This reference will be used to get the contry's color.
+    */
+   Commander mOwner;
+
+   /**
+    * mArmies keeps track of how many armies are in a country and will regulate
+    * the armies battle.
+    */
+   int mArmies;
+
 }
