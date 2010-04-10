7d5377ecf0e53a73297c5925085951806a36dd1f
Brandon Lyons <t.brandon.lyons@gmail.com>
2010-04-10 09:43:55 -0600

    Added member variable mHand that represents the cards the player has earned Added member variable mColor that represents the player's color

--- src/tabaar/Commander.java
+++ src/tabaar/Commander.java
@@ -1,10 +1,23 @@
 package tabaar;
 
+import java.awt.Color;
+import java.util.ArrayList;
+
 /**
- *
+ * Commander will be a superclass for the different players in the game.
  * @author tblyons
  */
 public class Commander
 {
+
+   /**
+    * mHand holds the Cards the Commander has accumulated
+    */
+   ArrayList<Card> mHand;
+
+   /**
+    * mColor represents the unique color of this player
+    */
+   Color mColor;
 
 }
