eac0cf52ded9f73a3139de586e51c9b8e515fa48
Brandon Lyons <t.brandon.lyons@gmail.com>
2010-04-10 09:48:26 -0600

    Added member variable mCountry that holds the country for this card Added member variable mType that holds the card type Added a default constructor to set mType which is a final TODO: consider setting mCountry as final as well because it should not change

--- src/tabaar/Card.java
+++ src/tabaar/Card.java
@@ -7,4 +7,21 @@ package tabaar;
 public class Card
 {
 
+   /**
+    * mCountry holds a reference to the Country this card is tied to.
+    */
+   Country mCountry;
+
+   /**
+    * mType keeps track of what type this card is.
+    */
+   final int mType;
+
+   /**
+    * The default constructor should never be used.
+    */
+   public Card()
+   {
+      mType = 0;
+   }
 }
