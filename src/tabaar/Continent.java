2a5786c259a8c59225dfb301b062ea7816ced8f5
Brandon Lyons <t.brandon.lyons@gmail.com>
2010-04-10 09:35:40 -0600

    Added member variable mBonus that will give the bonus for completely controlling the continent Added member variable mCountries that holds the countries that are on the continent Added a default constructor that sets the bonus to zero, because the bonus is a final and needs to be set

--- src/tabaar/Continent.java
+++ src/tabaar/Continent.java
@@ -1,5 +1,7 @@
 package tabaar;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author tblyons
@@ -7,4 +9,21 @@ package tabaar;
 public class Continent
 {
 
+   /**
+    * mBonus sets the Continent's bonus.
+    */
+   final int mBonus;
+
+   /**
+    * mCountries contains the Countries that make up the Continent
+    */
+   ArrayList<Country> mCountries;
+
+   /**
+    * The default constructor should never be used.
+    */
+   public Continent()
+   {
+      mBonus = 0;
+   }
 }
